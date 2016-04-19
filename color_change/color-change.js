e7a43cb2537e798f4729dfb61268511fe248110c
Jayendra <jay08jog@gmail.com>
2016-04-19 14:42:12 -0700

    Google background color change implemented

--- color_change/color-change.js
+++ color_change/color-change.js
@@ -1,1 +1,37 @@
-document.style.color = "red";
+function make_red()
+{
+	chrome.tabs.executeScript(null, {
+		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "red"'
+	});
+}
+function make_green()
+{
+	chrome.tabs.executeScript(null, {
+		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "green"'
+	});
+}
+function make_blue()
+{
+	chrome.tabs.executeScript(null, {
+		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "blue"'
+	});
+}
+function make_yellow()
+{
+	chrome.tabs.executeScript(null, {
+		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "yellow"'
+	});
+}
+function reset()
+{
+	chrome.tabs.executeScript(null, {
+		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "white"'
+	});
+}
+window.onload = function(){
+	document.getElementById('red').addEventListener('click', make_red);
+	document.getElementById('green').addEventListener('click', make_green);
+	document.getElementById('blue').addEventListener('click', make_blue);
+	document.getElementById('yellow').addEventListener('click', make_yellow);
+	document.getElementById('reset').addEventListener('click', reset);
+}
