927ad76f3d1563e7f472b4ef19c464c3db66d17a
Sharon Grewal <sharongrewal@users.noreply.github.com>
2016-05-24 12:25:36 -0700

    Update content.js

--- FB_hideNewsfeed/content.js
+++ FB_hideNewsfeed/content.js
@@ -10,18 +10,20 @@ function blockAndDisplay() {
     var feed = $('[id^=topnews_main_stream], [id^=mostrecent_main_stream], [id^=pagelet_home_stream]');
     var message = $('#distracted');
     console.log(message);
+    feed.children().remove();
+    $('#pagelet_games_rhc').remove();
+    
     if (feed.length == 0) {
         message.remove();
     } else if (message.length == 0) {
         NewsFeedReplacement(feed);
      }
 
-    feed.children().remove();
-    $('#pagelet_games_rhc').remove();
+
     // $('.ticker_stream').remove();
     // $('.ego_column').remove();
     // $('#pagelet_trending_tags_and_topics').remove();
     // $('#pagelet_canvas_nav_content').remove();
 }
-
-window.setInterval(blockAndDisplay, 100);
+blockAndDisplay();
+//window.setInterval(blockAndDisplay, 100);
