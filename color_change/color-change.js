d049fb42f98fee62962ecb9264194dda5260842c
Mark Tai <mark@marktai.com>
2017-02-06 15:35:59 -0800

    moving to jquery

--- color_change/color-change.js
+++ color_change/color-change.js
@@ -1,37 +1,38 @@
 function make_red()
 {
+	console.log('hi');
 	chrome.tabs.executeScript(null, {
-		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "red"'
+		code: '$("body").style.backgroundColor = "red"'
 	});
 }
 function make_green()
 {
 	chrome.tabs.executeScript(null, {
-		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "green"'
+		code: '$("body").style.backgroundColor = "green"'
 	});
 }
 function make_blue()
 {
 	chrome.tabs.executeScript(null, {
-		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "blue"'
+		code: '$("body").style.backgroundColor = "blue"'
 	});
 }
 function make_yellow()
 {
 	chrome.tabs.executeScript(null, {
-		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "yellow"'
+		code: '$("body").style.backgroundColor = "yellow"'
 	});
 }
 function reset()
 {
 	chrome.tabs.executeScript(null, {
-		code: 'document.getElementsByTagName("Body")[0].style.backgroundColor = "white"'
+		code: '$("body").style.backgroundColor = "white"'
 	});
 }
 window.onload = function(){
-	document.getElementById('red').addEventListener('click', make_red);
-	document.getElementById('green').addEventListener('click', make_green);
-	document.getElementById('blue').addEventListener('click', make_blue);
-	document.getElementById('yellow').addEventListener('click', make_yellow);
-	document.getElementById('reset').addEventListener('click', reset);
+	$('#red').addEventListener('click', make_red);
+	$('#green').addEventListener('click', make_green);
+	$('#blue').addEventListener('click', make_blue);
+	$('#yellow').addEventListener('click', make_yellow);
+	$('#reset').addEventListener('click', reset);
 }
